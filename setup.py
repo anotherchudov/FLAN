1c9c69b61453caf00e7b6842f5a539e184b6f8fd
anotherchudov <94729746+anotherchudov@users.noreply.github.com>
2023-03-02 02:29:19 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='flan',
     version='0.1.0',
-    packages=find_packages(include=['flan', 'flan.*'])
+    packages=find_packages(include=['flan', 'flan.*']),
     include_package_data=True
 )
