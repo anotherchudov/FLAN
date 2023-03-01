6865eed55a12d48df5a5372d93dcc50afe5c3a12
anotherchudov <94729746+anotherchudov@users.noreply.github.com>
2023-03-02 02:35:10 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='flan',
     version='0.1.0',
     packages=find_packages(include=['flan', 'flan.*']),
+    package_data={'': ['v2/cot_data/*.tsv']},
     include_package_data=True
 )
